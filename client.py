f86ee07211ae2cf2d68d0f813a9020c3a3bc8dd1
sumedh-github <71013130+sumedh-github@users.noreply.github.com>
2022-03-20 19:53:16 +0530

    Update client.py

--- client.py
+++ client.py
@@ -2,151 +2,227 @@ import socket
 import time
 import os
 
-def design(ip,port):
-    print("-" * 50)
-    print("Creating a client socket...")
-    print(f"Connecting to {ip}:{port}")
-    print("(*)Sucessful")
-    print("-" * 50)
-    print('* Note-Username login is not mandatory. Its just for log purpose\n\
-    Press ENTER for Guest login or just enter your name,it doesnt really matter.\n\
-    After logging in enter cmd "help" or "?" For more info...\n')
+op = 0
+pos = 0
+paswd_list = ['admin123']
+helpp = ' For basic arithmatics, Usage - [operand1][operator][operand2] eg 2+2 or 6/3...\n\
+ Other commands,\n\
+  1.exit       --Log out of current session.\n\
+  2.end        --End the current session(Closes the client program.)\n\
+  3.Terminate  --Shut the server down(Requires administration access).\n\
+  4.clear      --Clear the output screen.\n\
+  5.psswd      --Change admin password.'
 
-os.system("clear")
+def validate(username,cli_sock,paswd_list,pos):
+    cli_sock.send(bytes('Enter Passwd : ','utf-8'))
+    try:
+        passwd = cli_sock.recv(1024)
+    except KeyboardInterrupt as k:
+        flag = 'Terminate'
+        print('  [OK]Force Stop!')
+        return flag
+    if passwd.decode('utf-8') == paswd_list[pos]:
+        cli_sock.send(bytes('Authentication Sucessfull.','utf-8'))
+        return 'auth'
+    else:
+        cli_sock.send(bytes('Authentication Unsucessfull.','utf-8'))
+        print("  [.]Admin Authentication unsuccessfull.")
+        return 'unauth'
+
+def saviour(cli_msg,op):
+    for x in cli_msg:
+        if x == "+" or x == "-" or x == "*" or x == "/":
+            op = x       
+    if op == 0:
+        return 0,0,0
+    elif op == '+'or op == "-" or op == "*" or op == "/":
+        li = list(cli_msg.split(op))
+        op1 = li[0]
+        op2 = li[1]
+
+        try:
+            v = int(op1)
+            vv= int(op2)
+        except ValueError as V:
+            return 0,0,0
+
+        return op,op1,op2    
+
+def command_list(cli_msg,flag,username,paswd_list):
+    if cli_msg == 'exit':
+        close = 'Adios...'
+        cli_sock.send(bytes(close,'utf-8'))
+        print("  [.]Closed")
+        flag ='exit'
+        return flag
+    if cli_msg == 'end':
+        close = 'Session Terminated.'
+        cli_sock.send(bytes(close,'utf-8'))
+        print("  [.]Closed")
+        flag='end'
+        return flag
+    if cli_msg == 'help' or cli_msg == '?':
+        cli_sock.send(helpp.encode('utf-8'))
+        flag = 'helpp'
+        return flag
+    if cli_msg[:5] == 'psswd':
+        if username == 'Admin' or username == 'admin':
+            paswd_list.pop(0)
+            temp_pass = str(cli_msg[6:])
+            paswd_list.append(str(cli_msg[6:]))
+            cli_sock.send(bytes('Password changed sucessfully.','utf-8'))
+            print(f'  [.]{username} changed the password.')
+            flag = 'psswd'
+            return flag
+        else:
+            close = 'Unauthorised command.'
+            cli_sock.send(bytes(close,'utf-8'))
+            flag = 'unauth_user'
+            return flag
+            
+        
+    if cli_msg == 'Terminate':
+        if username == 'Admin' or username == 'admin':
+            confirmation = 'Are you sure you want to shutdown the server?(y/n) : '
+            cli_sock.send(bytes(confirmation,'utf-8'))
+            confirmation = cli_sock.recv(1024).decode('utf-8')
+            if confirmation == 'y':
+                close = 'Bye Admin.'
+                cli_sock.send(bytes(close,'utf-8'))
+                print("  [.]Closed")
+                print('[-]Shutting Server down.')
+                cli_sock.close()
+                time.sleep(0.5)
+                s.close()
+                print('\n[OK]Server down.')
+                flag ='Terminate'
+                return flag
+            elif confirmation == 'n':
+                flag = 'not_terminate'
+                return flag
+        else:
+            close = 'Unauthorised command.'
+            cli_sock.send(bytes(close,'utf-8'))
+            flag = 'unauth_user'
+            return flag
+        
+os.system("clear") 
 
 ip = '192.168.0.11'
 port = 5000
+error =  'Ambigious command.'
+
 print("-" * 50)
-print("Creating a client socket...")
+print(f"Creating a server socket... {ip}:{port}")
 time.sleep(1)
-print(f"Connecting to {ip}:{port}")
-print("(*)Sucessful")
-print("-" * 50)
-time.sleep(1)
+print("Done setting up the port.")
+print("-" *50)
 
-flag = False
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 
-print('* Note-Username login is not mandatory. Its just for log purpose\n\
-    Press ENTER for Guest login or just enter your name,it doesnt really matter.\n\
-    After logging in enter cmd "help" or "?" For more info...\n')
+try:
+    s.bind((ip,port))
+except OSError as o:
+    print("Port occupied. Don't press Ctrl+z you idiot. Use Ctrl+c insted.\nChange the port number form source code (server & client both) & Try again.")
+    exit()
 
+s.listen(5)
+
+time.sleep(2)
 while True:
-    time.sleep(2)
-
-    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-
-    try:
-        s.connect((ip,port))
-    except ConnectionRefusedError as c:
-        print('[-]Server Not Found.')
-        break
+    flag = ''
+    if flag == '':
+        print("[*]Waiting for the hosts to connect...")
 
     try:
-        username = input("Username : ")
+        cli_sock,cli_add = s.accept()
     except KeyboardInterrupt as k:
-        print('\nForced stop!')
+        print('\n[OK]Force Stop!')
         break
 
-    if username == '':
-        username = 'Guest'
-    
-    username = username.encode('utf-8')
-    s.send(username) 
-    
-    if username.decode('utf-8') == 'Admin' or username.decode('utf-8') == 'admin':
-        passpr = s.recv(1024).decode('utf-8')
-        print(passpr)
-        if passpr == 'Enter Passwd : ':
-            try:
-                passwd = input("--> ")
-            except KeyboardInterrupt as k:
-                print('Force Stop!')
-                s.send(bytes('null','utf-8'))
-                auth_ack = s.recv(1024).decode('utf-8')
-                print('[' + auth_ack + ']')
-                print('\n')
-                continue
-            if passwd == '':
-                s.send(bytes('null','utf-8'))
-                auth_ack = s.recv(1024).decode('utf-8')
-                print('[' + auth_ack + ']')
-                print('\n')
-                continue
-                
-            s.send(passwd.encode('utf-8'))
-            auth_ack = s.recv(1024).decode('utf-8')
-            if auth_ack == 'Authentication Sucessfull.':
-                print('[' + auth_ack + ']')
-                print('\n')
-            else :
-                print('[' + auth_ack + ']')
-                print('\n')
-                continue
+    try:
+        username = cli_sock.recv(1024) 
+    except KeyboardInterrupt as k:
+        print("Force Stop!")
+        break
+    if not username:
+        print('  [.]Connection terminated unexpectedly.')
+        continue
 
+    username = username.decode('utf-8')
 
-    msg = s.recv(1024) 
+    if username == 'Admin' or username == 'admin':
+        print(f"  [.]{username} login authenticating.")
+        flag = validate(username,cli_sock,paswd_list,pos)
+
+    if flag == 'unauth':
+        continue
+    if flag == 'Terminate':
+        break
+
+    print(f"  [.]{username} logged in.")
+    time.sleep(1)
+    msg = f"Welcome to the server {username}"
+
+    cli_sock.send((bytes(msg,'utf-8'))) 
+    
     while True:
-        if username.decode('utf-8') == 'Admin' or username.decode('utf-8') == 'admin':
-            print(f"{username.decode('utf-8')}@Server-[#] ",end=" ")
-        else:
-            print(f"{username.decode('utf-8')}@Server-[@] ",end=" ")
-
         try:
-            msg = input()
+            cli_msg = cli_sock.recv(1024)
         except KeyboardInterrupt as k:
-            flag = True
-            print('\nForced stop!')
+            print('\n  [OK]Force Stop!')
+            flag = 'Terminate'
             break
 
-        if msg == '':
-            continue
+        cli_msg = cli_msg.decode('utf-8')
 
-        if msg == 'clear':
-            os.system('clear')
-            design(ip,port)
+        flag = command_list(cli_msg,flag,username,paswd_list)
+
+        if flag == 'helpp':
             continue
+        if flag == 'exit':
+            break
+        if flag == 'end':
+            break
+        if flag == 'unauth_user':
+            continue
+        if flag == 'not_terminate':
+            continue
+        if flag == 'psswd':
+            continue
+        if flag == 'Terminate':
+            break
         
-        msg = msg.encode("utf-8")
+        operation,op1,op2 = saviour(cli_msg,op)
 
         try:
-            s.send(msg)
-        except BrokenPipeError as b:
-            print('Server Not Responding...')
+            if operation == 0 :
+                cli_sock.send(bytes('Ambiguos command.','utf-8'))
+                continue
+        except BrokenPipeError as B:
+            print('  [.]Connection terminated unexpectedly.')
             break
 
-        res = s.recv(1024)
-        print(res.decode('utf-8'))
-
-        if msg.decode('utf-8') == 'help' or msg.decode('utf-8') == '?':
-            print('\n')
-            continue
-        if res.decode('utf-8') == 'Adios...':
-            print("\n")
-            s.close()
-            break
-        if res.decode('utf-8') == 'Session Terminated.':
-            s.close()
-            exit()
-            break
-        if res.decode('utf-8') == 'Are you sure you want to shutdown the server?(y/n) : ':
-            try:
-                confirmation = input()
-            except KeyboardInterrupt as k:
-                print("Force Stop!")
+        op1 = int(op1)
+        op2 = int(op2)
+        
+        if operation == '+':
+            res = op1+op2
+        if operation == '-':
+            res = op1-op2
+        if operation == '*':
+            res = op1*op2
+        if operation == '/':
+            if op2 == 0:
+                res = 'Invalid input.'
+                cli_sock.send((bytes(res,'utf-8')))
                 continue
-            if confirmation == 'y':
-                s.send(confirmation.encode('utf-8'))
-                s.recv(1024) #Bye sumedh.
-                s.close()
-                exit()
-            elif confirmation == 'n':
-                s.send(confirmation.encode('utf-8'))
-                continue
-            break
-        if res.decode('utf-8') == 'Unauthorised command.':
-            continue
-        if res.decode('utf-8') == 'Ambigious command.':
-            continue
-    if flag:
+            else:
+                res = op1/op2
+        res = f'{op1}{operation}{op2}={res}'    
+        cli_sock.send((bytes(res,'utf-8')))
+    if flag == 'Terminate':
         break
+s.close()
+
